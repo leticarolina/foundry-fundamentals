2460d99db48089ea958d518f8b392e21bc75e040
Leticia Azevedo <109385942+leticarolina@users.noreply.github.com>
2024-11-26 22:57:47 -0300

    foundry install

--- basics.sol
+++
@@ -1,4 +0,0 @@
-//helpful commands 
-
-//clear or command k = to clear terminal
-// In the terminal "X" will hide terminal and the trash icon will delete the terminal flow.
---
+++ foundry-basics/basics.sol
@@ -0,0 +1,16 @@
+//helpful commands 
+
+//clear or command k = to clear terminal
+// In the terminal "X" will hide terminal and the trash icon will delete the terminal flow.
+//command + W = remove most recent block of code written in the terminak
+//command + U = Delete entire line of code 
+//command + backticks or J =  toogle terminal
+
+//creating a new folder = mkdir FolderName
+//when you type cd + the name of the folder can type first letters and hit tab to autocomplete
+
+//foundry comes with 4 components
+//forge
+//cast
+//anvil
+//chisal
