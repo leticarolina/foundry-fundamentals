d20cc97b5bb9278032a087295cc1efe958c38c18
Leticia Azevedo <109385942+leticarolina@users.noreply.github.com>
2024-11-27 22:39:53 -0300

    deploying with anvil from the command line

--- foundry-basics/basics.sol
+++ foundry-basics/basics.sol
@@ -1,16 +1,24 @@
-//helpful commands 
-
-//clear or command k = to clear terminal
-// In the terminal "X" will hide terminal and the trash icon will delete the terminal flow.
-//command + W = remove most recent block of code written in the terminak
-//command + U = Delete entire line of code 
-//command + backticks or J =  toogle terminal
-
-//creating a new folder = mkdir FolderName
-//when you type cd + the name of the folder can type first letters and hit tab to autocomplete
-
 //foundry comes with 4 components
 //forge
 //cast
 //anvil
 //chisal
+
+//========== FORGE
+//forge --help = all forge commands
+//forge init = Sets up a new minimal, smart contract project using the foundry development framework.
+//forge build or forge compile = compile the code aka makes machine readable
+//anvil - Create a local testnet node for deploying and testing smart contracts. It can also be used to fork other EVM compatible networks.
+
+//DEPLYING A SMART CONTRACT FROM THE TERMINAL
+//forge create - Deploy a smart contract.
+//Example full deploy of 'Simple storage
+forge create <path>:<contractname> --rpc-url 127.0.0.1:8545 --interactive
+//then it will prompt to insert private key
+//my first deplyment details on anvil
+Deployer: 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266
+Deployed to: 0x5FbDB2315678afecb367f032d93F642f64180aa3
+Transaction hash: 0x2149ff2c85a2c7fb1e2b9e971d7c20f2dc81fcb5b37e39843f883914be6573d7
+//the most explicit way to deply on anvil (but NEVER post private key special real one like that in plain text)
+// forge create SimpleStorage --rpc-url http://127.0.0.1:8545 --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
+
---
+++ foundry-basics/simple-storage/src/SimpleStorage.sol
@@ -0,0 +1,25 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.28;
+
+contract SimpleStorage {
+    struct Person {
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    Person[] public listOfPeople;
+
+    // Declare a mapping that links a keyType name (string) to a value: favorite number (uint256)
+    //syntax mapping(key -> type) visibility mappingName;
+    mapping(string => uint256) public giveNameGetFavoriteNumber;
+
+    //function to add a new person to the list and update the mapping
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        // Add a new Person struct to the array with the provided name and favorite number
+        listOfPeople.push(Person(_favoriteNumber, _name));
+
+        // Update the mapping with the person's name as the key and their favorite number as the value
+        giveNameGetFavoriteNumber[_name] = _favoriteNumber;
+    }
+}
