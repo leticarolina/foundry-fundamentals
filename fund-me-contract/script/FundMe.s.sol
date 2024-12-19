0c19e9754a58b9940836de9b1356a367172b2266
Leticia Azevedo <109385942+leticarolina@users.noreply.github.com>
2024-12-18 23:57:38 -0300

    mock contract for others networks

--- fund-me-contract/script/FundMe.s.sol
+++ fund-me-contract/script/FundMe.s.sol
@@ -3,13 +3,15 @@ pragma solidity ^0.8.18;
 
 import {Script} from "forge-std/Script.sol";
 import {FundMe} from "../src/fund-me.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract FundMeScript is Script {
+    HelperConfig helperConfig = new HelperConfig();
+    address ethUsdPriceFeed = helperConfig.activeNetworkConfig();
+
     function run() external returns (FundMe) {
         vm.startBroadcast();
-        FundMe deployedFundMe = new FundMe(
-            0x6D41d1dc818112880b40e26BD6FD347E41008eDA
-        );
+        FundMe deployedFundMe = new FundMe(ethUsdPriceFeed);
         vm.stopBroadcast();
         return deployedFundMe;
     }
--- fund-me-contract/script/HelperConfig.s.sol
+++ fund-me-contract/script/HelperConfig.s.sol
@@ -1,10 +1,36 @@
 // SPDX-License-Identifier: MIT
+//1.deploy mocks when we are on a local anvil chain
+//2.keep track of contract address accross different chains
 
 pragma solidity 0.8.19;
 
 import {Script} from "forge-std/Script.sol";
 
 contract HelperConfig {
-    // If we are on a local Anvil, we deploy the mocks
-    // Else, grab the existing address from the live network
+    // If we are on a local Anvil, we deploy the mocks // Else, grab the existing address from the live network
+    NetworkConfig public activeNetworkConfig;
+
+    struct NetworkConfig {
+        address priceFeed; //ETH/USD price feed address
+    }
+
+    constructor() {
+        if (block.chainid == 11155111) {
+            activeNetworkConfig = getSepoliaEthConfig();
+        } else {
+            activeNetworkConfig = GetAnvilEthConfig();
+        }
+    }
+
+    function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
+        //price feed adddress
+        NetworkConfig memory sepoliaConfig = NetworkConfig({
+            priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        });
+        return sepoliaConfig;
+    }
+
+    function GetAnvilEthConfig() public pure returns (NetworkConfig memory) {
+        //price feed adddress
+    }
 }
