71b514562a2f8a1e50bf4202e39e6e0246f124c3
Leticia Azevedo <109385942+leticarolina@users.noreply.github.com>
2024-12-29 12:57:06 -0300

    more tests

--- fund-me-contract/src/fund-me.sol
+++ fund-me-contract/src/fund-me.sol
@@ -15,10 +15,10 @@ error fundMe__notOwner();
 contract FundMe {
     uint256 funderIndex;
     uint256 public constant MINIMUM_VALUE_USD = 5e18; //Variables declared as constant are set at the time of compilation and cannot be changed afterward.
-    address[] public listOfAddressSentMoney;
+    address[] private s_listOfAddressSentMoney; //The s_ prefix stands for storage variables, which are written to or read from the Ethereum blockchain's persistent storage.
     //Mappings Are Like Hash Tables key to value
     mapping(address addressOfSender => uint256 amountSent)
-        public addressToAmountSent; //This line means that for each address key, there’s an associated uint256 value.
+        private s_addressToAmountSent; //This line means that for each address key, there’s an associated uint256 value.
     address public immutable i_owner; //Variables declared as immutable are set once, but only at deployment time, and cannot be changed afterward.
     AggregatorV3Interface private s_priceFeed;
 
@@ -41,23 +41,23 @@ contract FundMe {
             msg.value.getConversionRate(s_priceFeed) >= MINIMUM_VALUE_USD,
             "If the value is less than required then pop this message"
         );
-        listOfAddressSentMoney.push(msg.sender);
-        addressToAmountSent[msg.sender] =
-            addressToAmountSent[msg.sender] +
+        s_listOfAddressSentMoney.push(msg.sender);
+        s_addressToAmountSent[msg.sender] =
+            s_addressToAmountSent[msg.sender] +
             msg.value;
     }
 
     function withdraw() public CheckIfItsOwner {
         for (
             funderIndex = 0;
-            funderIndex < listOfAddressSentMoney.length;
+            funderIndex < s_listOfAddressSentMoney.length;
             funderIndex++
         ) {
-            address funder = listOfAddressSentMoney[funderIndex];
-            addressToAmountSent[funder] = 0; //This sets the amount sent by each address to 0, "withdrawing" their funds.
+            address funder = s_listOfAddressSentMoney[funderIndex];
+            s_addressToAmountSent[funder] = 0; //This sets the amount sent by each address to 0, "withdrawing" their funds.
         }
 
-        listOfAddressSentMoney = new address[](0);
+        s_listOfAddressSentMoney = new address[](0);
 
         (bool callSuccess, ) = payable(msg.sender).call{
             value: address(this).balance
@@ -75,5 +75,25 @@ contract FundMe {
     //Receives Ether along with data, or attempts to call a function that doesn’t exist in the contract.
     fallback() external payable {
         getFunds();
+    }
+
+    // view/pure functions (getters)
+    // Getter function to access the private mapping since the mapping is declared as private it wont have a getter function automatically
+    //because your drawer (the mapping) is private, no one can open it directly from outside the contract.
+    //It takes one input: fundingAddress (the address of the sender you want to look up). view: This tells Solidity that the function only reads data (it doesn’t change anything in the contract).
+    function getAddressToAmountToAmountSent(
+        address fundingAddress
+    ) external view returns (uint256) {
+        //This is where the function goes into the private mapping (s_addressToAmountSent), finds the value stored for the fundingAddress, and gives it back.
+        return s_addressToAmountSent[fundingAddress];
+        //example to call this function: uint256 amount = myContract.getAddressToAmountSent(0xDE...456);
+    }
+
+    //taking an index and returning an address
+    // This function lets external users or contracts retrieve the address of a funder by specifying their position (index) in the array.
+    //example using address funder = myContract.getAddressOfFunder(1);
+
+    function getAddressOfFunder(uint256 index) external view returns (address) {
+        return s_listOfAddressSentMoney[index];
     }
 }
--- fund-me-contract/test/FundMeTest.t.sol
+++ fund-me-contract/test/FundMeTest.t.sol
@@ -7,10 +7,16 @@ import {FundMeScript} from "../script/FundMe.s.sol";
 
 contract FundMeTest is Test {
     FundMe fundMe;
+    uint256 constant ONE_ETH = 1e18;
+    address USER = makeAddr("user"); //makeAddr() is a Foundry utility function that generate a unique Ethereum address.
+
+    uint256 constant USER_INITIAL_BALANCE = 10e18;
 
     function setUp() external {
         FundMeScript deployFundMe = new FundMeScript();
         fundMe = deployFundMe.run();
+        vm.deal(USER, USER_INITIAL_BALANCE); //vm.deal Foundry cheatcode that allows you to directly set the balance of any address for testing purposes.
+        //syntax vm.deal(address, amount);
     }
 
     //here testing if the variable MINIMUM_VALUE_USD is actually returning 5
@@ -28,7 +34,26 @@ contract FundMeTest is Test {
         assertEq(fundMe.i_owner(), msg.sender);
     }
 
-    function testFundFailWithoutEnoughEth() {}
+    function testFail_WithoutEnoughEth() public {
+        // expectRevert is used in testing to ensure that your smart contract correctly rejects invalid or unauthorized inputs by reverting. It’s like saying:
+        // "I expect the next line to fail  because it's invalid. If it doesn't fail, the test is broken and something's wrong with my contract logic."
+        //expectRevert used to test that a specific function call or action in your smart contract fails and reverts as expected.
+        //If the line fails, the test passes (expected behavior). If the line succeeds, the test fails (unexpected behavior).
+        vm.expectRevert("Insufficient funds sent");
+        fundMe.getFunds();
+    }
+
+    function testFundUpdatesDataStructure() public {
+        vm.prank(USER); //the next transaction will be sent by USER aadress
+        //This line simulates someone funding the FundMe contract with 1 ETH (in wei).
+        fundMe.getFunds{value: ONE_ETH}();
+        //Calls the getAddressToAmountToAmountSent function to check how much ETH the current contract (test contract) has sent to fundMe.
+        uint256 amountFunded = fundMe.getAddressToAmountToAmountSent(
+            address(USER)
+        );
+        //Using Foundry’s assertEq function to verify that the amountFunded matches 1e18 (1 ETH).
+        assertEq(amountFunded, ONE_ETH);
+    }
 }
 
 //forge test = to run the test file
