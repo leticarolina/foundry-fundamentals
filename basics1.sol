51afa56c02e934a66e630e0e13fb2cefdbc7f8ae
Leticia Azevedo <leticiac@email.com>
2025-06-17 13:56:25 -0700

    foundry starting to recap

---
+++ basics1.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+//----------------FOUNDRY-------------------
+//foundry comes with 4 components
+//forge - Compile, Deploy and test smart contracts.
+//cast - Interact with smart contracts and the Ethereum network.
+//anvil - Create a local testnet node for deploying and testing smart contracts. It can also be used to fork other EVM compatible networks.
+//chisal - A tool for formatting and linting Solidity code.
+
+//========== FORGE
+//forge --help = all forge commands
+//forge init = Sets up a new minimal, smart contract project using the foundry development framework (with src,test,cache)
+//forge build/forge compile = compile the code aka makes machine readable
+//forge fmt = will auto format the solidity code
+//forge test = run tests in the test folder
+
+//========== CAST
+
+//========== ANVIL
+
+//========== CHISAL
